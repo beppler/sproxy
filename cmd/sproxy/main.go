f448bfff7aeb954f2deb7707016b9c30b412c0ea
Carlos Alberto Costa Beppler <beppler@gmail.com>
2026-07-22 16:26:21 -0300

    Reorganize sproxy command source.

--- cmd/sproxy/main.go
+++ cmd/sproxy/main.go
@@ -16,61 +16,62 @@ import (
 )
 
 func main() {
-	logger := slog.New(middleware.NewRequestIdHandler(slog.Default().Handler()))
+	// configure options
+	var address, configurationFile, pacFile string
 
-	address := flag.String("address", "localhost:1357", "address to listen on")
-	configuration := flag.String("configuration", "wg0.conf", "path to wireguard configuration file")
-	proxyPac := flag.String("proxy-pac", "", "path to proxy.pac file")
+	flag.StringVar(&address, "address", "localhost:1357", "address to listen on")
+	flag.StringVar(&configurationFile, "configuration", "wg0.conf", "path to wireguard configuration file")
+	flag.StringVar(&pacFile, "proxy-pac", "", "path to proxy.pac file")
 
 	flag.Parse()
 
-	proxy, err := sproxy.NewProxyFromFile(logger, *configuration, *proxyPac)
+	logger := slog.New(middleware.NewRequestIdHandler(slog.Default().Handler()))
+
+	logger.Info("starting proxy", "address", address, "configuration", configurationFile, "proxy-pac", pacFile)
+
+	// configure sproxy handler, logging and request id middlewares
+	proxy, err := sproxy.NewProxyFromFile(logger, configurationFile, pacFile)
 	if err != nil {
-		logger.Error("error creating proxy", "error", err)
+		logger.Error("error configuring proxy", "error", err)
 		os.Exit(1)
 	}
-
-	handler := middleware.NewRequestIdMiddleware(
-		middleware.NewLoggingMiddleware(
-			proxy,
-			logger,
-		),
-		false,
-	)
-
-	logger.Info("starting service", "address", *address)
-
-	listener, err := net.Listen("tcp", *address)
-	if err != nil {
-		logger.Error("error starting server", "error", err)
-		os.Exit(1)
-	}
-
-	logger.Info("service started")
+	logging := middleware.NewLoggingMiddleware(proxy, logger)
+	handler := middleware.NewRequestIdMiddleware(logging, false)
 
 	server := &http.Server{
+		Addr:         address,
 		Handler:      handler,
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
 	}
 
+	// handle gracefull stop
 	serverStopped := make(chan any)
 
 	go func() {
 		stopAsked := make(chan os.Signal, 1)
 		signal.Notify(stopAsked, os.Interrupt)
 		<-stopAsked
-		logger.Info("stopping server")
+		logger.Info("stopping proxy")
 		if err := server.Shutdown(context.Background()); err != nil {
-			logger.Error("error stopping server", "error", err)
+			logger.Error("error stopping proxy", "error", err)
 		}
 		close(serverStopped)
 	}()
 
+	// serve sproxy requests
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		logger.Error("error starting proxy", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("proxy started")
+
 	if err := server.Serve(listener); !errors.Is(err, http.ErrServerClosed) {
-		logger.Error("error running server", "error", err)
+		logger.Error("error running proxy", "error", err)
 		os.Exit(1)
 	}
 
 	<-serverStopped
-	logger.Info("service stopped")
+	logger.Info("proxy stopped")
 }
